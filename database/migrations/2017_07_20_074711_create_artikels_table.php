54763b4805576eb06face7364719394d61c4cbc8
151610218-smkassalaambandung <151610218@smkassalaambandung.sch.id>
2017-07-20 15:59:00 +0700

    perubahan

---
+++ database/migrations/2017_07_20_074711_create_artikels_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArtikelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('artikels', function (Blueprint $table) {
+            $table->increments('id');
+            $table->increments('judul');
+            $table->increments('konten');
+            $table->increments('author_id')->unsigned();
+            $table->increments('gambar')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('artikels');
+    }
+}
